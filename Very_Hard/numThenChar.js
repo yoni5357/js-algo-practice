966450b5f1831b416b63b4ba9da9da73b5dda4ca
Yonatan Gorovetz <goro5357@gmail.com>
2025-09-04 14:30:25 +0300

    inum then char

--- Very_Hard/numThenChar.js
+++ Very_Hard/numThenChar.js
@@ -30,8 +30,39 @@ Notes
 Test cases will containg integer and float numbers and single letters.
 */
 
-function numThenChar( /*args*/ ) {
-  //your code
+function numThenChar(arrs) {
+  const sorted = flatenSort(arrs);
+  let k = 0;
+  for (let i = 0; i < arrs.length; i++){  
+    for(let j = 0; j < arrs[i].length;j++){
+      arrs[i][j] = sorted[k];
+      k++;
+    }
+  }
+  return arrs;
 }
+
+function flatenSort(arrs){
+  const flatArr = arrs.flat();
+  flatArr.sort((a,b) => {
+    if(typeof a === "number" && typeof b === "number"){
+      return a-b;
+    }
+    else if(typeof a === "string" && typeof b === "number"){
+      return 1;
+    }
+    else if(typeof a === "number" && typeof b === "string"){
+      return -1;
+    }
+    else{
+      return a.charCodeAt(0)-b.charCodeAt(0);
+    }
+  })
+  return flatArr;
+}
+
+console.log(numThenChar( [[1, 2, 4, 3, "a", "b"],
+ [6, "c", 5], [7, "d"],
+ ["f", "e", 8]]));
 
 exports.solution = numThenChar;
